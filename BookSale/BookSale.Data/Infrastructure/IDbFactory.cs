53f7269cda125dc3c3cc4020935a4035de10bc63
Nguyen Van Cuong <nguyenvanloc.github@gmail.com>
2018-06-22 13:46:45 +0700

    fixed

--- BookSale/BookSale.Data/Infrastructure/IDbFactory.cs
+++ BookSale/BookSale.Data/Infrastructure/IDbFactory.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BookSale.Data.Infrastructure
 {
-    public interface IDbFactory : IDisposable
+    public interface IDBFactory
     {
-        BookSaleDbContext Init();
     }
 }
