0d2bfb535e750ef6a736d7171acad0b558b666db
Nguyen Van Cuong <nguyenvanloc.github@gmail.com>
2018-07-05 14:12:30 +0700

    fix

--- BookSale/BookSale.Model/Models/Product.cs
+++ BookSale/BookSale.Model/Models/Product.cs
@@ -45,6 +45,8 @@ namespace BookSale.Model.Models
 
         [MaxLength(256)]
         public string ProductStatus { set; get; }
+        [MaxLength(256)]
+        public string Description { set; get; }
 
         public int SupplyhouseID { set; get; }
         [ForeignKey("SupplyhouseID")]
